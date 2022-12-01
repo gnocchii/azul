44527e13165327e7c04d48df81452f8c37d256c5
bananaman4321 <117681053+bananaman4321@users.noreply.github.com>
2022-12-01 13:32:26 -0600

    Update FloorLine.java

--- Backend/FloorLine.java
+++ Backend/FloorLine.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 public class FloorLine {
     private ArrayList<Tile> FloorLines;
     private Boolean firstTile;
-    int pointDeduction;
 
     public FloorLine() {
         pointDeduction = 0;
@@ -32,11 +31,6 @@ public class FloorLine {
                 penalty += 2;
             else if (i < 7)
                 penalty += 3;
-           
-           // change if need to update when floorline bigger than 7
-            else
-                penalty +=1000000;
-            
         }
 
         return penalty;
