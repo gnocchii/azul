d4418711a431571e546309ef30d990bb77b7ff58
Ella :D <117680659+ellaawei@users.noreply.github.com>
2022-12-02 21:37:27 -0600

    Update Frame.java

--- Frontend/Frame.java
+++ Frontend/Frame.java
@@ -1,17 +1,16 @@
 import java.awt.*;
 import javax.swing.*;
 
-
-
-public class Frame extends JFrame {
+public class Frame extends JFrame 
+{
 	private static final int WIDTH = 1800;
 	private static final int HEIGHT = 1000;
 	
-
-	public Frame (String framename){
+	public Frame (String framename)
+	{
 		super(framename);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		add(new Panel());
+		add(new TilePanel());
 		setSize(WIDTH,HEIGHT);
 		setVisible(true);
 	}
