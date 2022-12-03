1fce355486c4d1e54716bcece711c7fe9cf4a83c
Ella :D <117680659+ellaawei@users.noreply.github.com>
2022-12-02 22:48:16 -0600

    Update TilePanel.java

--- Frontend/TilePanel.java
+++ Frontend/TilePanel.java
@@ -19,9 +19,9 @@ import javax.swing.SpringLayout;
 public class TilePanel extends JPanel
 {
     private ArrayList<BufferedImage> tilesImages;
-    private BufferedImage mainAzulBoard;
+    private BufferedImage mainAzulBoard, factoryImage;
 
-    public TilePanel() throws IOException
+    public TilePanel()
     {
     	try
     	{
@@ -33,6 +33,7 @@ public class TilePanel extends JPanel
     	        tilesImages.add(ImageIO.read(TilePanel.class.getResource("/images/Red_Tile.png")));
     	        tilesImages.add(ImageIO.read(TilePanel.class.getResource("/images/Yellow_Tile.png")));
     	     mainAzulBoard = ImageIO.read(TilePanel.class.getResource("/images/Azul Board.jpg"));
+    	     factoryImage = ImageIO.read(TilePanel.class.getResource("/images/Azul_Factory.png"));
     	}
     	catch(Exception E)
     	{
@@ -43,10 +44,30 @@ public class TilePanel extends JPanel
 	
 	public void paint (Graphics g) 
 	{
-		Font currentFont = g.getFont();
-		Font font = currentFont.deriveFont(currentFont.getSize() * 5.4F);
-		g.setFont(font);
-		g.drawImage(mainAzulBoard,60, 275, 550, 550, null);
-		g.drawString("PLAYER " /*player thats currently playing*/, 150 , 900);
+		g.setFont(new Font("Times New Roman", Font.BOLD, 50));
+		g.setColor(new Color(0, 68, 129));
+		g.drawImage(mainAzulBoard,60, 350, 500, 500, null);
+		g.drawString("PLAYER " /*player thats currently playing(n)*/, 125 , 900);
+		
+		g.drawImage(mainAzulBoard, 150, 20, 250, 250, null);
+		g.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		g.drawString("PLAYER " /*player 1+n*/, 175, 305);
+		
+		g.drawImage(mainAzulBoard, 650, 20, 250, 250, null);
+		g.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		g.drawString("PLAYER " /*player 1+n*/, 675, 305);
+		
+		g.drawImage(mainAzulBoard, 1150, 20, 250, 250, null);
+		g.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		g.drawString("PLAYER " /*player 1+n*/, 1175, 305);
+		
+		g.drawImage(factoryImage, 950, 300, 150, 150, null);
+		g.drawImage(factoryImage, 950, 750, 150, 150, null);
+		g.drawImage(factoryImage, 775, 350, 150, 150, null);
+		g.drawImage(factoryImage, 700, 525, 150, 150, null);
+		g.drawImage(factoryImage, 775, 700, 150, 150, null);
+		g.drawImage(factoryImage, 1125, 350, 150, 150, null);
+		g.drawImage(factoryImage, 1125, 700, 150, 150, null);
+		g.drawImage(factoryImage, 1200, 525, 150, 150, null);
 	}
 }
