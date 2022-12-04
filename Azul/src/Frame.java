657929b3460f3456bab3e18793ac94f7e65112b9
Ella :D <117680659+ellaawei@users.noreply.github.com>
2022-12-03 23:30:14 -0600

    Update Frame.java

--- Azul/src/Frame.java
+++ Azul/src/Frame.java
@@ -1,12 +1,14 @@
 import java.awt.*;
+import java.io.IOException;
+
 import javax.swing.*;
 
 public class Frame extends JFrame 
 {
-	private static final int WIDTH = 1800;
+	private static final int WIDTH = 1500;
 	private static final int HEIGHT = 1000;
 	
-	public Frame (String framename)
+	public Frame (String framename) throws IOException
 	{
 		super(framename);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
